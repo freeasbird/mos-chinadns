89fb408f43d83ef7fd5548d4ef28d2f41ae1efeb
IrineSistiana <49315432+IrineSistiana@users.noreply.github.com>
2020-02-26 23:47:34 +0800

    update build script

--- build.py
+++ build.py
@@ -1,19 +1,3 @@
-#     Copyright (C) 2020, IrineSistiana
-#
-#     This file is part of mos-chinadns.
-#
-#     mosdns is free software: you can redistribute it and/or modify
-#     it under the terms of the GNU General Public License as published by
-#     the Free Software Foundation, either version 3 of the License, or
-#     (at your option) any later version.
-#
-#     mosdns is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU General Public License for more details.
-#
-#     You should have received a copy of the GNU General Public License
-#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # !/usr/bin/env python3
 import subprocess
@@ -24,11 +8,6 @@ import hashlib
 import logging
 
 project_name = 'mos-chinadns'
-main_path = './'
-CGO_ENABLED = '0'
-
-do_upx = True
-do_zip = True
 
 # [(env : value),(env : value)]
 env_combinations = [[['GOOS', 'darwin'], ['GOARCH', 'amd64']],
@@ -41,8 +20,10 @@ env_combinations = [[['GOOS', 'darwin'], ['GOARCH', 'amd64']],
 
                     # [['GOOS', 'linux'], ['GOARCH', 'mips'], ['GOMIPS', 'hardfloat']],
                     # [['GOOS', 'linux'], ['GOARCH', 'mips'], ['GOMIPS', 'softfloat']],
-                    #[['GOOS', 'linux'], ['GOARCH', 'mipsle'], ['GOMIPS', 'hardfloat']],
-                    [['GOOS', 'linux'], ['GOARCH', 'mipsle'], ['GOMIPS', 'softfloat']],
+                    # [['GOOS', 'linux'], ['GOARCH', 'mipsle'],
+                    #     ['GOMIPS', 'hardfloat']],
+                    # [['GOOS', 'linux'], ['GOARCH', 'mipsle'],
+                    #     ['GOMIPS', 'softfloat']],
 
                     # [['GOOS', 'linux'], ['GOARCH', 'mips64'],
                     #  ['GOMIPS64', 'hardfloat']],
@@ -54,12 +35,11 @@ env_combinations = [[['GOOS', 'darwin'], ['GOARCH', 'amd64']],
                     #  ['GOMIPS64', 'softfloat']],
 
                     # [['GOOS', 'freebsd'], ['GOARCH', '386']],
-                    # [['GOOS', 'freebsd'], ['GOARCH', 'amd64']],
+                    [['GOOS', 'freebsd'], ['GOARCH', 'amd64']],
 
                     # [['GOOS', 'windows'], ['GOARCH', '386']],
                     [['GOOS', 'windows'], ['GOARCH', 'amd64']],
                     ]
-
 
 
 def go_build():
@@ -77,40 +57,24 @@ def go_build():
         for pairs in env:
             os_env[pairs[0]] = pairs[1]  # add env
             zip_name = zip_name + '-' + pairs[1]
-        os_env['CGO_ENABLED'] = CGO_ENABLED
 
-        binary_name = project_name + ('.exe' if os_env['GOOS'] == 'windows' else '')
+        suffix = '.exe' if os_env['GOOS'] == 'windows' else ''
 
         logging.warning(f'building {zip_name}')
         try:
-            go_build_ldflags = '-s -w'
-            go_build_other_args = ''
+            subprocess.check_call(
+                f'go build -ldflags "-s -w"  -o {project_name}{suffix}', shell=True, env=os_env)
+            try:
+                subprocess.check_call(f'upx -9 -q {project_name}{suffix}', shell=True, stderr=subprocess.DEVNULL,
+                                      stdout=subprocess.DEVNULL)
+            except subprocess.CalledProcessError as e:
+                logging.error(f'upx failed: {e.args}')
 
-            buildCmd = f'go build -ldflags "{go_build_ldflags}" {go_build_other_args} -o {binary_name} {main_path}'
-            subprocess.check_call(buildCmd, shell=True, env=os_env)
-            upx_path = 'upx'
-            if do_upx:
-                try:
-                    subprocess.check_call(f'{upx_path} -9 -q {binary_name}', shell=True, stderr=subprocess.DEVNULL,
-                                          stdout=subprocess.DEVNULL)
-                except subprocess.CalledProcessError as e:
-                    logging.error(f'upx failed: {e.args}')
-
-            zip_includes = []
-            if do_zip:
-                with zipfile.ZipFile(f'{zip_name}.zip', mode='w', compression=zipfile.ZIP_DEFLATED,
-                                     compresslevel=1) as zf:
-                    for path in zip_includes:
-                        if os.path.isdir(path):  # path is a dir
-                            for dirpath, dirnames, filenames in os.walk('common'):
-                                for filename in filenames:
-                                    zf.write(os.path.join(dirpath, filename), filename)
-                        elif os.path.isfile(path):
-                            zf.write(path, os.path.basename(path))
-                        else:
-                            logging.warning(f'path is not a dir nor a file: {path}')
-
-                    zf.write(binary_name)
+            with zipfile.ZipFile(f'{zip_name}.zip', mode='w', compression=zipfile.ZIP_DEFLATED,
+                                 compresslevel=5  ) as zf:
+                zf.write(f'{project_name}{suffix}')
+                zf.write(f'README.md')
+                zf.write(f'LICENSE')
 
         except subprocess.CalledProcessError as e:
             logging.error(f'build {zip_name} failed: {e.args}')
